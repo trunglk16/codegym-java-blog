f654974f57689871e5c5f445fc459fae11cd9b6e
trunglk16 <trungnguyenlk16@gmail.com>
2019-01-09 09:37:08 +0700

    First

---
+++ src/main/java/trungnt/blog/model/Blog.java
@@ -0,0 +1,47 @@
+package trungnt.blog.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "blog")
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String tittle;
+    private String content;
+
+
+    public Blog() {
+    }
+
+    public Blog(String tittle, String content) {
+        this.tittle = tittle;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/trungnt/blog/repository/BlogRepository.java
@@ -0,0 +1,6 @@
+//package trungnt.blog.repository;
+//
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//
+//public class BlogRepository extends PagingAndSortingRepository<Blog> {
+//}
